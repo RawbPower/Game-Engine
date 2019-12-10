c98f9c11c6287c9a7c50542b6f41f6e0bd788cbf
Rob Power <robjamespower@gmail.com>
2019-12-10 10:31:32 +0000

    Adding Glad

--- Game-Engine/src/Platform/Windows/WindowsWindow.cpp
+++ Game-Engine/src/Platform/Windows/WindowsWindow.cpp
@@ -5,6 +5,8 @@
 #include "GameEngine/Events/MouseEvent.h"
 #include "GameEngine/Events/KeyEvent.h"
 
+#include <glad/glad.h>
+
 namespace ge {
 
 	static bool s_GLFWInitialized = false;
@@ -48,6 +50,8 @@ namespace ge {
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
 		glfwMakeContextCurrent(m_Window);
+		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+		GE_CORE_ASSERT(status, "Failed to initialize Glad!");
 		// A pointer to our Windows data struct
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
--- premake5.lua
+++ premake5.lua
@@ -13,8 +13,10 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 -- Include directories relative to root folder (solution directory)
 IncludeDir = {}
 IncludeDir["GLFW"] = "Game-Engine/vendor/GLFW/include"
+IncludeDir["Glad"] = "Game-Engine/vendor/Glad/include"
 
 include "Game-Engine/vendor/GLFW"
+include "Game-Engine/vendor/Glad"
 
 project "Game-Engine"
 	location "Game-Engine"
@@ -37,12 +39,14 @@ project "Game-Engine"
 	{
 		"%{prj.name}/src",
 		"%{prj.name}/vendor/spdlog/include",
-		"%{IncludeDir.GLFW}"
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}"
 	}
 
 	links
 	{
 		"GLFW",
+		"Glad",
 		"opengl32.lib"
 	}
 
@@ -54,7 +58,8 @@ project "Game-Engine"
 		defines
 		{
 			"GE_PLATFORM_WINDOWS",
-			"GE_BUILD_DLL"
+			"GE_BUILD_DLL",
+			"GLFW_INCLUDE_NONE"
 		}
 
 		postbuildcommands
